6ebc673d5ffc6a61b30618bcc0d1c2a31a804b9c
jhonnygut <jhonny.gut@gmail.com>
2016-01-21 11:50:44 -0300

    seleccionar planta, centro de negocio, trabajador

--- src/js/layout.js
+++ src/js/layout.js
@@ -20,25 +20,34 @@ dojo.require("dojo.number");
 dojo.require("dojo.data.ObjectStore");
 dojo.require("dojo.store.Memory");
 
+
+//Edificaciones/Areas con su riesgos:
+// http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&layers=est40516:Edificacion&styles=&bbox=-71.59356418337775,-35.609444896273274,-71.5824568379388,-35.605179045497955&width=768&height=330&srs=EPSG:4326&format=application/openlayers
+// http://104.196.40.15:8080/geoserver/est40516/wms?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetLegendGraphic&LAYER=est40516:Edificacion&transparent=TRUE"
+//<img alt="legend" src="http://104.196.40.15:8080/geoserver/est40516/wms?REQUEST=GetLegendGraphic&VERSION=1.1.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=est40516:Edificacion" />
+
+//people:
+// http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&layers=est40516:people&styles=&bbox=808860.7101161322,6054190.876024606,809239.8662073202,6054295.726372454&width=768&height=330&srs=EPSG:32718&format=application/openlayers
+
 /*-----------------------------------------------------------------------------------------*/
 /*  ------------------------------         INIT()          ------------------------------  */
 /*-----------------------------------------------------------------------------------------*/
 
+var mapas = {};
 function init() {
 	//Generamos formulario de seleccion
 	Slct();
 
-	//crear DIV
-	getCreate();
+	//crear logo en el mapa
+	getLogo();
 
-	//var map = L.map('map', {center: [-35.607,-71.588],zoom: 16});
-	var map = L.map('map', {center: [-36.8395,-73.1139],zoom: 6});
+	mapas.central = L.map('map', {center: [-36.3,-72.3],zoom: 8});
 
-	//Edificaciones/Areas con su riesgos:
-	//http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&layers=est40516:Edificacion&styles=&bbox=-71.59356418337775,-35.609444896273274,-71.5824568379388,-35.605179045497955&width=768&height=330&srs=EPSG:4326&format=application/openlayers
+	//mapas.PlantaMaule = L.map('map', {center: [-35.607,-71.588],zoom: 16}); //Planta Maule
+	//mapas.ENAP = L.map('map', {center: [-36.780,-73.125],zoom: 15}); //Enap
+	//mapas.OficEST = L.map('map', {center: [-36.8395,-73.114],zoom: 18}); //Oficina EST
+	//mapas.Chile = L.map('map', {center: [-37,-73],zoom: 4}); //Chile
 
-	//people:
-	//http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&layers=est40516:people&styles=&bbox=808860.7101161322,6054190.876024606,809239.8662073202,6054295.726372454&width=768&height=330&srs=EPSG:32718&format=application/openlayers
 	var url = 'http://104.196.40.15:8080/geoserver/est40516/wms';
 	
 	// for all possible values and explanations see "Template Parameters" in https://msdn.microsoft.com/en-us/library/ff701716.aspx
@@ -48,27 +57,30 @@ function init() {
 	var ggl = new L.Google();
 	var gglH = new L.Google('HYBRID');
 
-	map.addLayer(ggl);
-	map.addLayer(gglH);
-	//map.addLayer(bing);
-	//map.addControl(new L.Control.Layers( {'Bing':bing, 'Google':ggl, 'Google Hibrido':gglH}, {}));
-	map.addControl(new L.Control.Layers( {'Google':ggl, 'Google Hibrido':gglH}, {}));
+	//mapas.central.addLayer(bing);
+	mapas.central.addLayer(gglH);
+	//mapas.central.addLayer(ggl);
+	//mapas.central.addControl(new L.Control.Layers( {'Bing':bing, 'Google':ggl, 'Google Hibrido':gglH}, {}));
+
+	mapas.central.addControl(new L.Control.Layers( {'Google':ggl, 'Google Hibrido':gglH}, {}));
+	//L.Control.Layers( {'Google Hibrido':gglH, 'Google':ggl}, {}).addTo(mapas.central);
 
 	L.tileLayer.betterWms(url, {
 		layers: 'est40516:Edificacion, est40516:people',
 		transparent: true,
 		format: 'image/png'
-		}).addTo(map);
+		}).addTo(mapas.central);
 
-	/*L.tileLayer.betterWms(url, {
+	/** /
+	L.tileLayer.betterWms(url, {
 		layers: 'est40516:people',
 		transparent: true,
 		format: 'image/png'
-		}).addTo(map);
-
+		}).addTo(mapas.central);
+	/**/
+	dojo.create('div', null, dojo.byId('leyenda'), "first");
 	/*FILTROS*/
 	dojo.connect(dijit.byId("planta"), "onChange", actFiltros);
-
 	/*fin*/
 	}
 
@@ -78,14 +90,14 @@ function init() {
 
 
 //Filtros...
-function actFiltros(valores) {
+function actFiltros(valor) {
 	//vectores de Filtro
 	var vecPlanta = dijit.byId("planta").get("value");
 	console.log(vecPlanta);
-
-	var map2 = L.map('map2', {center: [-35.607,-71.588],zoom: 16});
-
-	map.sync(map2);
+	console.log(valor);
+	if(valor === '*'){mapas.central.setView([-36.3,-72.3], 8);}
+	if(valor === '01'){mapas.central.setView([-35.607,-71.588], 16);}
+	if(valor === '02'){mapas.central.setView([-36.780,-73.125], 15);}
 	}
 
 
@@ -170,9 +182,11 @@ function Slct() {
 	}
 
 //Crear DIV para logo en el mapa
-function getCreate() {	
+function getLogo() {	
 	dojo.create('img', {src: './images/logo-estchile.png'}, dojo.byId('logoMap'));
 	}
 
 //dojo ready init
 dojo.ready(init);
+
+
