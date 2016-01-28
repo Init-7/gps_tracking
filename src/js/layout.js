c811c7fa0466f576ad72b9f0ea35e493e2068224
jhonnygut <jhonny.gut@gmail.com>
2016-01-28 19:39:38 -0300

    info trabajador v0.1

--- src/js/layout.js
+++ src/js/layout.js
@@ -20,19 +20,14 @@ dojo.require("dojo.number");
 dojo.require("dojo.data.ObjectStore");
 dojo.require("dojo.store.Memory");
 
-
-//Edificaciones/Areas con su riesgos:
-// http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&styles=&bbox=-71.59356418337775,-35.609444896273274,-71.5824568379388,-35.605179045497955&width=768&height=330&srs=EPSG:4326&format=application/openlayers&layers=est40516:Edificacion
-
-//people:
-// http://104.196.40.15:8080/geoserver/est40516/wms?service=WMS&version=1.1.0&request=GetMap&styles=&bbox=808860.7101161322,6054190.876024606,809239.8662073202,6054295.726372454&width=768&height=330&srs=EPSG:32718&format=application/openlayers&layers=est40516:people
-
 /*-----------------------------------------------------------------------------------------*/
 /*  ------------------------------         INIT()          ------------------------------  */
 /*-----------------------------------------------------------------------------------------*/
 
 var db = {};
+
 var mapas = {};
+var layer = [];
 
 var url = {};
            url.central = 'http://104.196.40.15:8080/geoserver/est40516/wms';
@@ -43,7 +38,26 @@ url.leyendaEdificacion = 'http://104.196.40.15:8080/geoserver/est40516/wms?REQUE
 function init() {
 	//Generamos formulario de seleccion
 	Slct();
+	//cargamos el mapa
+	addMap();
+	//cargamos capas
+	addLayer()
+	/*genera leyenda*/
+	getLegend();
 
+	/*FILTROS*/
+	dojo.connect(dijit.byId("planta"), "onChange", actFiltros);
+	dojo.connect(dijit.byId("centro"), "onChange", actFiltros);
+	dojo.connect(dijit.byId("trabajador"), "onChange", actFiltros);
+	/*fin*/
+	}
+
+/*-----------------------------------------------------------------------------------------*/
+/*  ------------------------------         Slct()          ------------------------------  */
+/*-----------------------------------------------------------------------------------------*/
+
+//Filtros...
+function addMap() {
 	mapas.central = L.map('map', {center: [-36.3,-72.3],zoom: 8});
 
 	//mapas.PlantaMaule = L.map('map', {center: [-35.607,-71.588],zoom: 16}); //Planta Maule
@@ -64,61 +78,63 @@ function init() {
 	//mapas.central.addControl(new L.Control.Layers( {'Bing':bing, 'Google':ggl, 'Google Hibrido':gglH}, {}));
 	mapas.central.addControl(new L.Control.Layers( {'Google':ggl, 'Google Hibrido':gglH}, {}));
 	//L.Control.Layers( {'Google Hibrido':gglH, 'Google':ggl}, {}).addTo(mapas.central);
-
-	L.tileLayer.wms(url.central, {
-		layers: 'est40516:Edificacion',
-		transparent: true,
-		format: 'image/png',
-		styles: 'PMaule'
-		}).addTo(mapas.central);
-
-	/**/
-	L.tileLayer.wms(url.central, {
-		layers: 'est40516:people',
-		transparent: true,
-		format: 'image/png',
-		styles: 'Trabajador'
-		}).addTo(mapas.central);
-
-	/*genera leyenda*/
-	getLegend();
-
-	/*FILTROS*/
-	dojo.connect(dijit.byId("planta"), "onChange", actFiltros);
-	dojo.connect(dijit.byId("centro"), "onChange", actFiltros);
-	dojo.connect(dijit.byId("trabajador"), "onChange", actFiltros);
-	/*fin*/
 	}
 
-/*-----------------------------------------------------------------------------------------*/
-/*  ------------------------------         Slct()          ------------------------------  */
-/*-----------------------------------------------------------------------------------------*/
+//agregamos capas
+function addLayer() {    
+    layer.maule = L.tileLayer.wms(url.central, {
+        layers: 'est40516:Edificacion',
+        transparent: true,
+        format: 'image/png',
+        styles: 'PMaule',
+        attribution: 'Edificacion'
+        });
+    layer.maule.options.crs = L.CRS.EPSG4326;
+    layer.maule.setOpacity(1)
+    layer.maule.addTo(mapas.central);
+    /**/
+    layer.job =  L.tileLayer.wms(url.central, {
+        layers: 'est40516:people',
+        transparent: true,
+        format: 'image/png',
+        styles: 'Trabajador',
+        attribution: 'Trabajador'
+        });
+    layer.job.options.crs = L.CRS.EPSG4326;
+    layer.job.setOpacity(1)
+    layer.job.addTo(mapas.central);
 
+    mapas.central.off('click', ShowWMSLayersInfo);
+    mapas.central.on('click', ShowWMSLayersInfo); 
+	}
 
 //Filtros...
 function actFiltros(valor) {
-	//vectores de Filtro
-	var vecPlanta = dijit.byId("planta").get("value");
-	console.log(vecPlanta);
-	if(vecPlanta === '*'){mapas.central.setView([-36.3,-72.3], 8);
+	db.vecPlanta = dijit.byId("planta").get("value");
+
+	if(db.plantaSelect === '*'){mapas.central.setView([-36.3,-72.3], 8);
 		dojo.attr(dojo.byId('work'), "src", url.leyendaEdificacion);
 		}
-	if(vecPlanta === '01'){
+	if(db.plantaSelect === '01'){
 		mapas.central.setView([-35.607,-71.588], 16);
 		dojo.attr(dojo.byId('work'), "src", url.leyendaPMaule);
 		}
-	if(vecPlanta === '02'){mapas.central.setView([-36.780,-73.125], 15);
+	if(db.plantaSelect === '02'){mapas.central.setView([-36.780,-73.125], 15);
+		dojo.attr(dojo.byId('work'), "src", url.leyendaEdificacion);
+		}
+	if(db.plantaSelect === '03'){mapas.central.setView([-36.8395,-73.114], 18);
 		dojo.attr(dojo.byId('work'), "src", url.leyendaEdificacion);
 		}
 	}
 
-
+//generamos select para filtros
 function Slct() {
 	db.plantas =  [
 		{ plant: "*", value: "*", name: "todas las plantas", selected: true },
 
 		{ plant: "01", value: "01", name: "CMPC-Planta Maule" },
 		{ plant: "02", value: "02", name: "ENAP" },
+		{ plant: "03", value: "03", name: "Oficina EST" },
 		];
 
 	db.centros =  [
@@ -126,6 +142,7 @@ function Slct() {
 
 		{ center: "01-201507", plant: "01", value: "01-201507", name: "Contrato Fibra Mecánico" },
 		{ center: "01-201508", plant: "01", value: "01-201508", name: "Contrato Bodega de Repuestos" },
+		{ center: "03-201601", plant: "03", value: "03-201601", name: "Pruebas GPS" },
 		];
 
 	db.trabajadores =  [
@@ -135,6 +152,9 @@ function Slct() {
 
 		{ job: "job-0506", center: "01-201508", plant: "01", value: "job-0506", name: "Juan Carlos Gonzalez" },
 		{ job: "job-0507", center: "01-201508", plant: "01", value: "job-0507", name: "Jorge Gajardo" },
+
+		{ job: "job-0506", center: "03-201601", plant: "03", value: "job-0506", name: "Lautaro Silva" },
+		{ job: "job-0507", center: "03-201601", plant: "03", value: "job-0507", name: "Jhonny Gutiérrez" },
 		];
 
 	new dijit.form.FilteringSelect({
@@ -146,8 +166,10 @@ function Slct() {
 		required: false,
 		style: "font-size:90%;",
 		onChange: function(plant){
+			db.plantaSelect = this.item.plant;
+			console.log('plant: ',db.plantaSelect);
 			if(this.item.plant != "*"){
-				dijit.byId('centro').query.plant = this.item.plant || /.*/;
+				dijit.byId('centro').query.plant = this.item.plant || "*" || /.*/;
 				dijit.byId('centro').set('value', this.item ? "*" : null);
 				dijit.byId('trabajador').query.plant = this.item.plant || /.*/;
 				dijit.byId('trabajador').set('value', this.item ? "*" : null);
@@ -170,6 +192,8 @@ function Slct() {
 		required: false,
 		style: "font-size:90%;",
 		onChange: function(center){
+			db.plantaSelect = this.item.plant;
+			console.log('center: ',db.plantaSelect);
 			if(this.item.center != "*"){
 				dijit.byId('trabajador').query.center = this.item.center || /.*/;
 				dijit.byId('trabajador').set('value', this.item ? "*" : null);
@@ -189,7 +213,11 @@ function Slct() {
 		required: false,
 		queryExpr: "*${0}*",
 		ignoreCase:true,
-		style: "font-size:90%;"
+		style: "font-size:90%;",
+		onChange: function(job){
+			db.plantaSelect = this.item.plant;
+			console.log('job: ',db.plantaSelect);
+			}
 		}, "trabajador");
 	}
 
@@ -199,6 +227,48 @@ function getLegend() {
 	dojo.create('br', null, dojo.byId('leyenda'));
 	dojo.create('img', {src: url.leyendaEdificacion,id:'work'}, dojo.byId('leyenda'));
 	}
+
+//info
+function ShowWMSLayersInfo(evt){
+    var urls = getFeatureInfoUrl(mapas.central,layer.job,evt.latlng,{'info_format': 'text/html'});
+	var inner = '<iframe src="' + urls + '" width="100%" height="110px" style="border:none"></iframe>'
+	dojo.attr(dojo.byId('itemDetails'), "innerHTML", inner);
+
+    console.log(urls);
+    }
+
+//URL para obtener detalles de capa WMS 
+function getFeatureInfoUrl(map, layer, latlng, params) {
+    var point = map.latLngToContainerPoint(latlng, map.getZoom()),
+        size = map.getSize(),
+        bounds = map.getBounds(),
+        sw = bounds.getSouthWest(),
+        ne = bounds.getNorthEast(),
+        sw = ([sw.lng, sw.lat]),
+        ne = ([ne.lng, ne.lat]);
+
+    var defaultParams = {
+        request: 'GetFeatureInfo',
+        service: 'WMS',
+        srs: layer._crs.code,
+        styles: '',
+        version: layer._wmsVersion,
+        format: layer.options.format,
+        bbox: [sw.join(','), ne.join(',')].join(','),
+        height: size.y,
+        width: size.x,
+        layers: layer.options.layers,
+        query_layers: layer.options.layers,
+        info_format: 'text/html'
+        };
+
+    params = L.Util.extend(defaultParams, params || {});
+
+    params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+    params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+
+    return layer._url + L.Util.getParamString(params, layer._url, true);
+    }
 
 //dojo ready init
 dojo.ready(init);
